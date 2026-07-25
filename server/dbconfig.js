e3842ec7addbaea3455339dc0998eda40a43be18
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-25 14:08:25 +0500

    Update dbconfig.js

--- server/dbconfig.js
+++ server/dbconfig.js
@@ -1,9 +1,10 @@
 import { MongoClient } from "mongodb";
-
+import dotenv from "dotenv";
+dotenv.config();
 const dbName = "task-manager";
 export const collectionName = "tasks";
-const url = "mongodb://127.0.0.1:27017";
-
+const url = process.env.MONGODB_URI;
+console.log(url);
 const client = new MongoClient(url);
 
 export const connection = async () => {
